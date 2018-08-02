a47ef563734841325fa0a677a198106ffaeff0a9
AkiraHakuta <ahkt9898@gmail.com>
2018-08-02 14:54:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ sys.argv.append('build')
 
 base = None
 includes = []
-include_files = ['gen', 'mod_gen', 'html2latex.csv', 'sample1.html', 'sample2.html', 'cat.png', 'readme.txt']
+include_files = ['gen', 'mod_gen', 'example', 'html2latex.csv', 'readme.txt']
 excludes = []
 packages = ['antlr4']
 executables = [cx_Freeze.Executable(script="antlr4_html2latex.py",targetName="antlr4_html2latex.exe", base = base),]
